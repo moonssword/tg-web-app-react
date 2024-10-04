fd8a4fc461703e9bcad7a42f299709c88797edcf
Aybulat Yuldashbaev <yuldashbaev@gmail.com>
2024-10-04 22:19:22 +0500

    v1.0.1

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "crypto": require.resolve("crypto-browserify"),
+    };
+    return config;
+};
